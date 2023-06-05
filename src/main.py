e51a43e90d156cfbd2faaac4b05b0ed026163f77
Yuri Rodrigues Teixeira <71528635+yuri3gp@users.noreply.github.com>
2023-06-05 13:05:50 -0300

    assets

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
 import tkinter as tk
+import ctypes
 from recorder import ScreenRecorder
+
+# Define o caminho para o arquivo de ícone
+# Substitua "icone.ico" pelo nome do seu arquivo de ícone
+icon_path = "assets/icone.ico"
+
+# Define o ícone do aplicativo
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("Screen Recorder")
+ctypes.windll.kernel32.SetConsoleIcon(ctypes.windll.LoadLibraryW(icon_path))
 
 # Iniciar o aplicativo
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Screen Recorder")
     root.geometry("400x200")
-    
+
     ScreenRecorder(root)
-    
+
     root.mainloop()
